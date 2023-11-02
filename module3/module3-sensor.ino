a50e8ef73f412f1f664339b05c92d996b59839d1
ExzoZbta <90164856+ExzoZbta@users.noreply.github.com>
2023-11-02 05:38:21 -0400

    Update module3-sensor.ino

--- module3/module3-sensor.ino
+++ module3/module3-sensor.ino
@@ -7,9 +7,10 @@
 
 // touch pin - Touch0 is T0 which is on GPIO 4.
 #define TOUCH_PIN 4
+// touch pin #2 - Touch2 is T2 which is on GPIO 2.
+#define TOUCH2_PIN 2
 
 const int touch_threshold = 35;
-int touch_value;
 
 WiFiUDP Udp;
 
@@ -36,14 +37,24 @@ void loop() {
 
   int light_val = analogRead(PHOTORESISTOR_PIN);
   int touch_val = touchRead(TOUCH_PIN);
+  int touch2_val = touchRead(TOUCH2_PIN);
   
   Serial.println(String("Touch Value: ") + touch_val);  // get value of Touch 0 pin = GPIO 4
+  Serial.println(String("Touch #2 Value: ") + touch2_val);  // get value of Touch 2 pin = GPIO 2
+
 
   if (touch_val < touch_threshold) {
     Serial.println("Wake The Eyes");
   }
   else {
     Serial.println("The Eyes are Calm");
+  }
+
+  if (touch2_val < touch_threshold) {
+    Serial.println("The Eyes Cry");
+  }
+  else {
+    Serial.println("The Eyes Rest");
   }
 
   // int beginPkt = Udp.beginPacket("172.29.32.91", 4200); // ip address changes for some reason
@@ -53,14 +64,19 @@ void loop() {
   // Serial.println(String("Begin Packet: ") + beginPkt);
 
   // Create separate UDP packets for light and touch values
-  Udp.beginPacket("172.29.32.91", 4200); // Change the IP address accordingly
+  Udp.beginPacket("172.29.132.18", 4200); // Change the IP address accordingly
   Udp.print("Light: ");
   Udp.println(light_val);
   Udp.endPacket();
 
-  Udp.beginPacket("172.29.32.91", 4200); // Change the IP address accordingly
+  Udp.beginPacket("172.29.132.18", 4200); // Change the IP address accordingly
   Udp.print("Touch Value: ");
   Udp.println(touch_val);
+  Udp.endPacket();
+
+  Udp.beginPacket("172.29.132.18", 4200); // Change the IP address accordingly
+  Udp.print("Touch#2 Value: ");
+  Udp.println(touch2_val);
   Udp.endPacket();
   
   // int pkt = Udp.endPacket();
